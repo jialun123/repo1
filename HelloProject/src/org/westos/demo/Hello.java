abac8c8fdfa6ac3275cc8ba923d83d6476fea025
Karen <13109563292@163.com>
2020-12-02 19:59:28 +0800

    将java工程添加到版本库

---
+++ HelloProject/src/org/westos/demo/Hello.java
@@ -0,0 +1,13 @@
+package org.westos.demo;
+
+/**
+ * @Author: JiaLun
+ * @Description:
+ * @Date: Created in 2020/12/2 19:49
+ * @Modified By:
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+}
